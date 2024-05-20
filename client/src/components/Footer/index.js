fd4b03036715dd1060347c89bb707e9b8e357297
jenstem <jennifer.stemkowski@gmail.com>
2024-05-20 14:03:15 -0400

    Add Header + Footer components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="w-100 mt-auto bg-secondary p-4">
+            <div className="container">&copy;2024 by Jen Stemkowski</div>
+        </footer>
+    );
+};
+
+export default Footer;
